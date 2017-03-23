20af6a97010fd8bd2cceb71973bb1fb87ab06148
Jee Hoon <yamoo9@naver.com>
2017-03-23 11:33:29 +0900

    JavaScript Data Types

---
+++ WEEK__1/JavaScript/es5.core.pattern.js
@@ -0,0 +1,46 @@
+/*! es5.core.pattern.js © yamoo9.net, 2017 */
+
+// 1. JavaScript 데이터 유형
+
+// 1-1. 원시(Primitive) 데이터 유형
+// 객체가 아닌 것(속성을 포함하지 않는다): undefined
+// 객체가 아닌 것(속성을 포함하지 않는다): null
+// 숫자 값(number literal)
+// 문자 값(string literal)
+// 논리 값(boolean literal)
+
+var num = 2017;
+var str = 'JavaScript';
+var boo = !0;
+
+// 1-2. 참조(Reference)형 데이터 유형
+// 객체 {}
+// 배열 객체 []
+// 함수 객체 function(){}
+var obj = {};
+var arr = [];
+var fnc = function() {};
+
+// 원시 데이터 타입, 참조형 데이터 타입을 구분하는 중요한 포인트!
+// 값 복사(pass by value)
+// 값 참조(pass by reference)
+
+// 자바스크립트 메모리 관리는 어떻게 하는가?
+
+// ---
+
+// 2. JavaScript 데이터 유형 올바른 감지
+
+// 3. JavaScript 구문과 표현식
+
+// 4. JavaScript 함수에 관한 이야기
+
+// 5. JavaScript 배열에 관한 이야기
+
+// 6. JavaScript 객체에 관한 이야기
+
+// 7. DOM API에 관한 이야기
+
+// 8. ECMAScript 2015
+
+// 9. AJAX & REST API
