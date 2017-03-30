03756e8265d418cdb043b34345fc24a1fd73a760
Jee Hoon <yamoo9@naver.com>
2017-03-30 16:40:50 +0900

    Card Component jQuery Control

--- WEEK__2/jQuery/es6/app.js
+++ WEEK__2/jQuery/es6/app.js
@@ -48,3 +48,54 @@
   });
 
 })(window, window.jQuery);
+
+
+(function(global, $){
+  'use strict';
+
+  // Card Component 제어
+  var $card = $('.card');
+
+  // 이벤트 위임
+  $card.on('click', 'a', function(e) {
+    // 기본 동작 차단
+    e.preventDefault();
+    var $this = $(this);
+    if ( $this.hasClass('is-toggle') ) {
+      toggleCardContentView($this);
+    }
+    if ( $this.hasClass('is-save') ) {
+      saveCardContent();
+    }
+    if ( $this.hasClass('is-edit') ) {
+      editCardContent();
+    }
+    if ( $this.hasClass('is-delete') ) {
+      removeCard($this);
+    }
+    // .card-button.is-toggle
+  });
+
+  // $card 관련된 메서드
+  function toggleCardContentView($el) {
+    var $icon    = $el.find('.fa');
+    var $content = $el.parents('.card').find('.card-content');
+    if ( $content.hasClass('is-hidden') ) {
+      // 버튼 뷰 변경
+      $el.attr('aria-label', 'card collapse');
+      // 버튼 아이콘 뷰 변경
+      $icon.removeClass('fa-angle-up').addClass('fa-angle-down');
+    } else {
+      $el.attr('aria-label', 'card expand');
+      $icon.removeClass('fa-angle-down').addClass('fa-angle-up');
+    }
+    // 카드 콘텐츠 뷰 변경
+    $content.toggleClass('is-hidden');
+  }
+  function removeCard($el) {
+    $el.parents('.card').remove();
+  }
+  function saveCardContent ($el) { }
+  function editCardContent ($el) { }
+
+})(window, window.jQuery);
