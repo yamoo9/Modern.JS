19eec5dd7ed67b0508633f6a9289ae52b3bca4f5
Jee Hoon <yamoo9@naver.com>
2017-03-30 15:32:53 +0900

    jQuery Plugin 유형 radioClass 학습

--- WEEK__2/jQuery/js/app.js
+++ WEEK__2/jQuery/js/app.js
@@ -1,11 +1,32 @@
 'use strict';
 
 // JavaScript Module Pattern (IIFE)
-;(function (global, Model) {
+;(function (global, $) {
   'use strict';
 
-  // create Model {} Instance(Object) from Model Class(Constructor)
+  var count = 101920;
 
-  global.lectureList = new Model([]);
-  global.readingBookList = new Model([]);
-})(window, window.Model);
+  // jQuery 확장 여부 확인
+  console.log('jQuery Version is', $.version);
+  console.log('jQuery.isNumber(count)', $.isNumber(count));
+  console.log('jQuery.isStandardMode:', $.isStandardMode);
+  console.log('jQuery.getPixelDensity():', $.getPixelDensity());
+  console.log('jQuery.isHiDPI():', $.isHiDPI());
+
+  console.log('$.isBoolean(count):', $.isBoolean(count));
+
+  var $wrapper_paras = $('.wrapper').find('p');
+
+  $.each($wrapper_paras, function (index) {
+    var $p = $wrapper_paras.eq(index);
+
+    // $p.on('click', function() {
+    //   $(this).radioClass('is-active');
+    // });
+
+    // jQuery
+    $p.on('click', $.proxy($.fn.radioClass, $p, 'is-active'));
+    // ES 5
+    // $p.on( 'click', $.fn.radioClass.bind($p) );
+  });
+})(window, window.jQuery);
