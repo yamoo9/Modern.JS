d53740442a6e33389512787c2a2b19ac205e2f3c
Jee Hoon <yamoo9@naver.com>
2017-04-06 18:10:22 +0900

    Vue CLI

---
+++ WEEK__3/vue-cli-study/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-brand :name="app_name"></app-brand>
+  </div>
+</template>
+
+<script>
+import AppBrand from './components/appBrand.vue'
+
+export default {
+  name: 'app',
+  components: {
+    appBrand: AppBrand
+  },
+  data () {
+    return {
+      app_name: 'Vue JS App'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+body
+  margin: 3rem
+  font-size: 2rem
+
+</style>
---
+++ WEEK__3/vue-cli-study/src/components/appBrand.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="app-brand is-active">{{name}}</h1>
+</template>
+
+<script>
+  export default {
+    props: ['name'],
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+$brand-color: #fc414b
+
+.app-brand
+  font-family: "Helvetica Neue"
+  color: $brand-color
+</style>
