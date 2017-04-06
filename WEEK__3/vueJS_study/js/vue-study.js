0f57a2aa9ad148e6fb7e2d02582fb6c2e8c650f1
Jee Hoon <yamoo9@naver.com>
2017-04-06 14:04:40 +0900

    Vue JS 조건/반복 구문 처리

--- WEEK__3/vueJS_study/js/vue-study.js
+++ WEEK__3/vueJS_study/js/vue-study.js
@@ -43,14 +43,53 @@ window.user = {
 }) // (window, window.jQuery);
 
 
-// 3. Vue Framework
+// 3. Vue Framework, ES5
+// Module Pattern: IIFE
 ;(function(global, Vue){
   'use strict';
 
-  // Vue 인스턴스(객체) 생성
-  global.vm = new Vue({
+  var vue_setting = {
+    // Vue 인스턴스가 관찰할 템플릿 요소 연결
     el: '.container',
-    data: global.user
-  });
+    // 모델 데이터 설정
+    // ⌘ 서버에서 가져온 데이터 연결
+    // data: global.user
+    data: {
+      input: 'First Commit',
+      using_v_text: false,
+      people: [
+        {
+          name: '조나단',
+          age: 32
+        },
+        {
+          name: '앤써니',
+          age: 26
+        },
+        {
+          name: '라라벨',
+          age: 45
+        }
+      ],
+      todoList: [
+        // 데이터 모델링
+        {
+          done: false,
+          todo: 'Vue.js 학습'
+        },
+        {
+          done: false,
+          todo: 'Vue.js TodoList 만들기'
+        },
+        {
+          done: true,
+          todo: 'jQuery 스타일 차차 잊기'
+        }
+      ]
+    }
+  };
+
+  // Vue 인스턴스(객체) 생성
+  global.vm = new Vue(vue_setting);
 
 })(window, window.Vue);
