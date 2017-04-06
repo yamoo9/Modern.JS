8fe3f15f1bfc6594c2b77cd8d8c39b672d3e26fa
Jee Hoon <yamoo9@naver.com>
2017-04-06 19:04:12 +0900

    Vue CLI 컴포넌트 분리

--- WEEK__3/vueJS_study/js/vue-component.js
+++ WEEK__3/vueJS_study/js/vue-component.js
@@ -3,7 +3,7 @@
 // Vue Component 정의 방법
 // 전역 정의
 // 컴포넌트의 이름은 커스텀 엘리먼트 이름이 된다.
-// <comp-name></comp-name>
+// <app-tabs></app-tabs>
 Vue.component('tabs', {
   template: `
     <div class="tabs">
@@ -11,27 +11,54 @@ Vue.component('tabs', {
         <slot></slot>
       </ul>
     </div>
-  `
+  `,
+  data: function() {
+    return {
+      active: 0
+    };
+  },
+  methods: {
+    processActive: function(i) {
+      console.log(i);
+    }
+  }
 });
 
 Vue.component('tab', {
   // 컴포넌트 정의
   // 부모 컴포넌트로부터 속성 전달
-  props: ['control'],
+  props: ['item', 'index'],
   // 템플릿 설정
   template: `
     <li role="presentation">
-      <a role="tab" class="tab" href> <slot></slot> </a>
+      <a
+        @click.prevent="tabActive(index)"
+        role="tab" href> {{item}} </a>
     </li>
   `,
+  data: function() {
+    return {}
+  },
   methods: {
-    tabActive: function() {
-      console.log('tab activation');
+    tabActive: function(index) {
+      // console.log(index);
+      this.$emit('active', index);
+      // a 를 사용자가 클릭했을 때
+      // 부모 li에 .is-active 상태 클래스를 적용
     }
   }
 });
 
 // 특정 컴포넌트 내에 종속(지역 정의)
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    tabs: [
+      'Computer',
+      'IA',
+      'AI',
+      'IOT',
+      'IT',
+    ]
+  }
 });
