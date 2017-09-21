f201c4fa775c44a22c2a823446b074b33db5d2b0
Jee Hoon <yamoo9@naver.com>
2017-09-21 15:20:31 +0900

    Front-End Develop 모듈 관리

--- WEEK__01/js/modules/dom.js
+++ WEEK__01/js/modules/dom.js
@@ -1,17 +1,24 @@
-function dom(el){
-  this.el = el;
-  // return this;
-}
+(function(global, y9){
+  // JavaScript Syntax Mode: Strict (ES5: 2009)
+  'use strict';
 
-dom.prototype = {
-  constructor: dom,
-  init: function(){
-    console.log('initialization');
-  },
-  bind: function(){
-    console.log('bind events');
-  },
-  update: function(){
-    console.log('update content');
+  // 비공개(Private)
+  // 생성자 함수(Class)
+  function Dom(el, settings) {
+    this.el = el;
+    this.config = settings;
+    // return this;
   }
-};
+
+  // 프로토타입 객체(Instance Sharing Methods, Properties)
+  Dom.prototype = {
+    constructor: Dom,
+    init: function(){console.log('initialization');},
+    bind: function(){console.log('bind Events');},
+    update: function(){console.log('Update Contents');}
+  };
+
+  // 외부에 공개할 것만 선별적으로 공개(Public) 가능
+  y9.Dom = Dom;
+
+})(window, window.y9 = window.y9 || {});
