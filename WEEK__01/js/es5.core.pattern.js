7a5cefd4466f3c493b12bf5a249627b37a5579bb
Jee Hoon <yamoo9@naver.com>
2017-09-21 13:00:02 +0900

    데이터 타입 검증

--- WEEK__01/js/es5.core.pattern.js
+++ WEEK__01/js/es5.core.pattern.js
@@ -42,6 +42,7 @@ var my_btn = new Button(); // Button {}
 // number
 // string
 // boolean
+console.groupCollapsed('값 복사와 참조');
 
 var numx = num; // = 할당 연산자
 
@@ -69,22 +70,90 @@ book_list.push('다시 만난 세계'); // ['다시 만난 세계']
 console.log('[fix] book_list:', book_list); // ['다시 만난 세계']
 console.log('[fix] arr:', arr);             // ['다시 만난 세계']
 
+console.groupEnd('값 복사와 참조');
+
+
 ////////////////////////////////////
 // 2. JavaScript 데이터 유형 올바른 감지
 ////////////////////////////////////
 
+// 동적 형 지정 언어 (JavaScript) VS 정적 형 지정 언어 (JAVA)
+// 태생적 한계 (많은 문제를 수반)
+
+// TS
+// function someMoney(money:number){
+// ...
+// }
+
+// JS
+// function someMoney(money){
+// 데이터 유효성 검증
+//   if (typeof money !== 'number') {
+//     throw new Error('매개변수 money는 숫자 유형만 받습니다.');
+//   }
+// }
+
 // 데이터 타입 검증 방법 1. typeof
 // http://bonsaiden.github.io/JavaScript-Garden/ko/#types.typeof
+
+console.log('typeof num:', typeof num);             // 'number'
+console.log('typeof str:', typeof str);             // 'string'
+console.log('typeof boo:', typeof boo);             // 'boolean'
+console.log('typeof fnc:', typeof fnc);             // 'function'
+console.log('typeof undefined:', typeof undefined); // 'undefined'
+console.log('typeof obj:', typeof obj);             // 'object'
+console.log('typeof arr:', typeof arr);             // 'array' [X]  -> 'object'
+console.log('typeof my_btn:', typeof my_btn);       // 'button' [X] -> 'object'
+console.log('typeof null:', typeof null);           // 'null' [X]   -> 'object'
+
+// TS
+// var num:number = 9;
+// var fnc:() = function(){};
 
 
 // 데이터 타입 검증 방법 2. instanceof
 // http://bonsaiden.github.io/JavaScript-Garden/ko/#types.instanceof
 
+// Class <-> Instance
+// 설계도면 <-> 생성된 객체
+// 거북선도면 <-> 거북선
+
+// new Class() => Instance
+
+arr instanceof Array;  // true, new Array()
+console.log('arr instanceof Array:', arr instanceof Array)
+num instanceof Number; // true, new Number()
+console.log('num instanceof Number:', num instanceof Number)
+
+
 
 // 데이터 타입 검증 방법 3. .consturctor 속성
 
+console.log('num.constructor:', num.constructor);
+console.log('str.constructor:', str.constructor);
+console.log('boo.constructor:', boo.constructor);
+console.log('fnc.constructor:', fnc.constructor);
+console.log('arr.constructor:', arr.constructor);
+console.log('obj.constructor:', obj.constructor);
+console.log('my_btn.constructor:', my_btn.constructor);
 
-// 데이터 타입 검증 방법 4. 직접 만든 유틸리티 함수 detectType
+
+// JavaScript의 데이터 검증을 위한 3가지 방법
+// typeof, instanceof, .constructor
+
+// 데이터 타입 검증 방법 4. 직접 만든 유틸리티 함수 isType
+// JavaScript 의 모든 데이터 유형을 올바르게 검증하려면?
+function type(data) {
+  return Object.prototype.toString.call(data).toLowerCase().slice(8,-1);
+}
+function isType(data, data_type){
+  // Boolean 반환
+  return type(data) === data_type;
+}
+// 문제가 되었던 Array, null, undefined 체크?
+isType(arr, 'array');           // true
+isType(null, 'null');           // true
+isType(undefined, 'undefined'); // true
 
 
 ///////////////////////////
