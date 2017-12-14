99c52c9806ace1a28c8a42c3c1a4b2ccea1b80fe
yamoo9 <yamoo9@naver.com>
2017-12-14 12:34:28 +0900

    jQuery 코드 ES6화

---
+++ WEEK__02/LECTURE/src/es6/main.js
@@ -0,0 +1,23 @@
+// ES6
+jQuery(document).ready($ => {
+
+    let buttons = $('button', '.toggle-radio-container');
+    let active_class = 'is-loading';
+    // ※ 화살표 함수 내부의 this는 무엇을 가리키나?
+    let toggleRadioButton = e => {
+        // console.log(this);
+        // 화살표 함수 내부의 this는
+        // 함수를 실행시킨 주체가 아니라,
+        // 함수의 상위 영역을 참조하는 객체이다.
+        // let $this = $(this);
+        let $this = $(e.target);
+        $this.siblings(`.${active_class}`).removeClass(active_class);
+        $this.addClass(active_class);
+    };
+    
+    buttons.each((index, button) => {
+        // this는 화살표 함수와 함께 사용할 때 기존과 다르게 동작한다.
+        $(button).on('click', toggleRadioButton);
+    });
+
+});
