6342fac9bbd1fc4aeaadea634af21fe4f7678070
yamoo9 <yamoo9@naver.com>
2017-12-14 14:48:23 +0900

    jQuery / ES6 / babel-polyfill / default parameter

--- WEEK__02/LECTURE/dist/js/main.js
+++ WEEK__02/LECTURE/dist/js/main.js
@@ -1,9 +1,43 @@
-'use strict';
+// 개발(배포) 의존 모듈 로드
+// CommonJS
+// require('babel-polyfill');
+// AMD
+// UMD
+// IIFE
+// ES
+// import "babel-polyfill";
 
 // ES6
 jQuery(document).ready(function ($) {
 
     var buttons = $('button', '.toggle-radio-container');
+
+    // jQuery 객체(리스트) => 배열
+    // jQuery 유틸리티 메서드 $.makeArray()
+
+    // 객체를 생성해야지만 사용 가능한 메서드
+    // 객체(인스턴스)가 사용할 수 있는 ....
+    // 인스턴스 메서드
+    // $().each()
+
+    // 객체를 생성하지 않고도 사용 가능한 메서드
+    // 객체가 아닌 경우에 사용합니다.
+    // 일반 함수, 네임스페이스 객체에 종속된 속성
+    // 클래스(스태틱) 메서드
+    // $.each()
+
+
+    // ES6 새롭게 추가된 객체
+    // iteratable object
+    // string, array, arguments, set, map, weakset, weakmap
+    // array => set
+    // object => map
+
+    var data = [34, 56, 78, 34, 90];
+    console.log(data);
+    var data_set = new Set(data); // unique
+    console.log(data_set);
+
     var active_class = 'is-loading';
     // ※ 화살표 함수 내부의 this는 무엇을 가리키나?
     var toggleRadioButton = function toggleRadioButton(e) {
@@ -29,3 +63,28 @@ jQuery(document).ready(function ($) {
         $(button).on('click', toggleRadioButton);
     });
 });
+
+// IIFE
+// ES5
+(function (global, $) {
+    // default parameter value setting
+    global = global || window;
+    $ = $ || window.jQuery;
+})();
+
+function checkRequirejQuery() {
+    if (!window.jQuery) {
+        throw new Error('required jQuery');
+    } else {
+        return window.jQuery;
+    }
+}
+
+// ES6
+// defualt parameter
+(function () {
+    var global = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window;
+    var $ = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : checkRequirejQuery();
+
+    $('body').addClass('using-jQuery');
+})();
