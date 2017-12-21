990b99b71855716c82760ed6a6037d4a24c482d1
Jee Hoon <yamoo9@naver.com>
2017-12-22 00:19:34 +0900

    3주차 교육 마무리

--- WEEK__03/LearnVue/STEP-3/webpack-vue-template/src/App.vue
+++ WEEK__03/LearnVue/STEP-3/webpack-vue-template/src/App.vue
@@ -1,60 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <bgVideo :message="msg"></bgVideo>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+
+  import bgVideo from './components/BgVideo.vue';
+
+  export default {
+    name: 'app',
+    components: { bgVideo },
+    data () {
+      return {
+        msg: '공부하기 쉬워요~ Vue~~'
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #eb6238;
+    margin-top: 120px;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ WEEK__03/LearnVue/STEP-3/webpack-vue-template/src/components/BgVideo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-video">
+    <p>{{message}}</p>
+    <iframe width="560" height="315" :src="ytdSource" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BgVideo',
+  props: [],
+  props: {
+    message: {
+      type: String
+    }
+  },
+  data() {
+    return  {
+      url: 'x0r_7koevhw'
+    }
+  },
+  computed: {
+    ytdSource() {
+      return `https://www.youtube.com/embed/${this.url}`;
+    }
+  }
+}
+</script>
+
